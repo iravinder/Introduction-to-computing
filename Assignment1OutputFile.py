95f835e70502bdead243b292f1b0c4e7d1609d81
RAVINDER KUMAR <96733863+iravinder@users.noreply.github.com>
2022-01-08 17:39:12 +0530

    Add files via upload

---
+++ Assignment1OutputFile.py
@@ -0,0 +1,38 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: /Users/ravinder/Downloads/Programs/AsQ1.py =============
+Enter the first number 24
+Enter the second number 36
+Enter the third number 60
+The average of the given numbers is 40.0
+
+============== RESTART: /Users/ravinder/Downloads/Programs/AsQ2.py =============
+Enter your gross income(in dollars) 500000
+Enter the number of dependents 4
+The amount of tax you have to pay is  95600.0 dollars
+
+============== RESTART: /Users/ravinder/Downloads/Programs/AsQ3.py =============
+Enter the student's SID 21103120
+Enter the student's name Ravinder Kumar
+Enter the student's age 18
+Enter the student's gender F/M M
+Enter the student's course name CSE
+Enter the student's CGPA 9.5
+[21103120, 'Ravinder Kumar', 18, 'M', 'CSE', 9.5]
+
+============== RESTART: /Users/ravinder/Downloads/Programs/AsQ4.py =============
+Enter the marks of first student 67
+Enter the marks of second student 88
+Enter the marks of third student 79
+Enter the marks of forth student 81
+Enter the marks of fifth student 59
+[59, 67, 79, 81, 88]
+
+============= RESTART: /Users/ravinder/Downloads/Programs/AsQ5a.py =============
+Prior color list:  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Color list after removal of the 4th element:  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+============= RESTART: /Users/ravinder/Downloads/Programs/AsQ5b.py =============
+Prior color list:  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Color list after replacing Black and Pink with Purple  ['Red', 'Green', 'White', 'Purple', 'Yellow']
