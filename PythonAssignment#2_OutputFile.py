e9f6ec8f91aada6071d25ef3bed956a31af47229
RAVINDER KUMAR <96733863+iravinder@users.noreply.github.com>
2022-01-22 19:43:30 +0530

    Add files via upload

---
+++ PythonAssignment#2_OutputFile.py
@@ -0,0 +1,47 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: /Users/ravinder/RAVINDER/Programs/Python/practice.py ========
+The length of the string is: 35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+The index of 'a' is: 10
+Pythonisacasesensitivelanguage
+
+========= RESTART: /Users/ravinder/RAVINDER/Programs/Python/practice.py ========
+Enter your name: Ravinder Kumar
+Enter your SID: 21103120
+Enter the name of your department: CSE
+Enter your CGPA: 9.9
+
+Hey, Ravinder Kumar Here!
+My SID is 21103120
+I am from CSE department and my CGPA is 9.9
+
+========= RESTART: /Users/ravinder/RAVINDER/Programs/Python/practice.py ========
+The value of a&b is: 8
+The value of a|b is: 58
+The value of a^b is: 50
+
+The value of 56 after left shift of 2 bits is: 224
+The value of 10 after left shift of 2 bits is: 40
+
+The value of 56 after right shift of 2 bits is: 14
+The value of 10 after right shift of 2 bits is: 2
+
+========= RESTART: /Users/ravinder/RAVINDER/Programs/Python/practice.py ========
+Enter the first number: 9
+Enter the second number: 6
+Enter the third number: 4
+9 is the greatest
+
+========= RESTART: /Users/ravinder/RAVINDER/Programs/Python/practice.py ========
+Enter the string: My name is Ravinder
+Yes, 'name' is present in the entered string
+
+========= RESTART: /Users/ravinder/RAVINDER/Programs/Python/practice.py ========
+Enter the value of the first side 7
+Enter the value of the second side 5
+Enter the value of the third side 9
+Yes, the given combination of sides form a triangle
---
+++ PythonAssignment#2_SourceCodeFile.py
@@ -0,0 +1,133 @@
+#Ravinder Kumar SID: 21103120
+#PythonAssignment#2
+
+#======================================== Question1 ========================================
+
+#String given in the question
+string = "Python is a case sensitive language"
+
+#a. Length of the input string
+print("The length of the string is:",len(string))
+
+#b. Reversing the order of the string
+print(string[::-1])
+
+#c. Storing "a case sensitive in a new string"
+string2 = string[10:26]
+print(string2)
+
+#d. Replacing "a case sensitive" with "object oriented"
+print(string.replace("a case sensitive", "object oriented"))
+
+#e. Finding the index of substring "a"
+print("The index of 'a' is:",string.find("a"))
+
+#f. Removing the spaces between the substrings
+print(string.replace(" ",""))
+
+
+#======================================== Question2 ========================================
+
+#Format provided in the question
+format = '''\nHey, ABC Here!
+My SID is 2110XXXX
+I am from XYZ department and my CGPA is X.X'''
+
+#Asking user for the credentials
+name = str(input("Enter your name: "))
+SID = str(input("Enter your SID: "))
+department = str(input("Enter the name of your department: "))
+CGPA = str(input("Enter your CGPA: "))
+
+#Replacing the required substrings as per the user input 
+format = format.replace("ABC", name)
+format = format.replace("2110XXXX", SID)
+format = format.replace("XYZ", department)
+format = format.replace("X.X", CGPA)
+
+print(format)
+
+
+#======================================== Question3 ========================================
+
+a=56
+b=10
+
+#a. Value of a&b
+print("The value of a&b is:",a&b)
+
+#b. Value of a|b
+print("The value of a|b is:",a|b)
+
+#c. Value of a^b
+print("The value of a^b is:",a^b)
+
+#d. Left shift both a & b with 2 bits
+print("\nThe value of",a,"after left shift of 2 bits is:",a<<2)
+print("The value of",b,"after left shift of 2 bits is:",b<<2)
+
+#e. Right shift both a & b with 2 bits
+print("\nThe value of",a,"after right shift of 2 bits is:",a>>2)
+print("The value of",b,"after right shift of 2 bits is:",b>>2)
+
+
+#======================================== Question4 ========================================
+
+#Asking input of three numbers from the user
+num1 = int(input("Enter the first number: ")) #first number
+num2 = int(input("Enter the second number: ")) #second number
+num3 = int(input("Enter the third number: ")) #third number
+
+#Applying conditions for the comparison
+if num1>=num2 and num1>=num3:
+    print(num1,"is the greatest")
+
+elif num2>=num1 and num2>=num3:
+    print(num2,"is the greatest")
+
+else:
+    print(num3,"is the greatest")
+
+
+#======================================== Question5 ========================================
+
+#Asking user for the string
+input_string = str(input("Enter the string: "))
+
+#length of the entered string
+length = len(input_string)
+
+#Index of the 'name' substring in the string entered by the user
+name_index = input_string.find("name")
+
+'''Since the indexing start from '0' in python then 
+the index of the last character in the entered string will be (length-1)'''
+
+#Applying condition whether the name_index lies in range of 0 to length-1
+if (name_index>=0 & name_index<=(length-1)):
+    print("Yes, 'name' is present in the entered string")
+
+else:
+    print("No, 'name' is not present in the entered string")
+
+
+#======================================== Question6 ========================================
+
+#Asking user for the value of the sides
+side1 = int(input("Enter the value of the first side "))
+side2 = int(input("Enter the value of the second side "))
+side3 = int(input("Enter the value of the third side "))
+
+#Conditions of sum of sides for a triangle are:-
+# (side1+side2)>side3
+# (side1+side3)>side2
+# (side2+side3)>side1
+
+if ((side1+side2)>side3 and (side1+side3)>side2 and (side2+side3)>side1):
+    print("Yes, the given combination of sides form a triangle")
+
+else:
+    print("No, the given combination of sides don't form a triangle")
+
+
+#end
